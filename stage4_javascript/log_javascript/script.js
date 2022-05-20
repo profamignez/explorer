2973821ccb47439b3ea8405710920deb474fb742
mignez <93382579+profamignez@users.noreply.github.com>
2022-05-20 10:26:45 -0300

    Stage 4 - Lógica e Javascript

---
+++ stage4_javascript/log_javascript/script.js
@@ -0,0 +1,14 @@
+// Comentário Linha
+/* Comentário multi linhas */
+
+
+//alert("Olá, Inez")
+
+/*
+let nome = prompt("Qual o seu nome?")
+alert("Olá, " + nome)
+*/
+
+
+let nome = prompt("Qual é o seu nome?")
+alert('Olá, ' + nome)
