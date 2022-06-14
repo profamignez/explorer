ba4a41cc651f02db24a983e045ae4cbf7a382b2e
mignez <93382579+profamignez@users.noreply.github.com>
2022-06-14 11:30:23 -0300

    Stage 4 - Revisão 1

---
+++ stage4_javascript/aula_revisao/notes.md
@@ -0,0 +1,10 @@
+Estrutura de Dados
+
+# Ferramentas da linguagem que abordaremos
+
+[] Estruturas de dados:
+ objetos
+ Função
+ Fluxo de Aplicação -
+ If, else if, else
+   switch
---
+++ stage4_javascript/aula_revisao/script.js
@@ -0,0 +1,35 @@
+// if, else if, else
+/*
+const isRainning = true
+if (true) {
+    alert("Está chovendo, pega o guarda-chuvas!")
+} else {
+    alert("Dia limpo" )
+}
+
+
+
+/*
+Faça uma estrutura de dados que contenha uma escolha
+
+Qual é o tipo da água?
+
+*/
+
+
+//switch
+const water = "sem gás"
+
+switch(water) {
+    case "com gás":
+alert("Essa água é com gás")
+break 
+case "sem gás":
+    alert("Essa água é sem gás")
+break
+    case "da fonte":
+alert("água fresquinha da fonte")  
+break
+default:
+alert ("cadê minha água?")
+}
