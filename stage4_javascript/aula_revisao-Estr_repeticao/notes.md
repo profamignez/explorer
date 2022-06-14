898358a9deb73e7fb7e58dd8694416349083c3e8
mignez <93382579+profamignez@users.noreply.github.com>
2022-06-14 11:48:46 -0300

    Stage 4 - Revisão 2

---
+++ stage4_javascript/aula_revisao-Estr_repeticao/notes.md
@@ -0,0 +1,9 @@
+Estrutura de Dados
+
+# Ferramentas da linguagem que abordaremos
+
+[] Estruturas de Repetição:
+ 
+- while
+- for
+- for...of
---
+++ stage4_javascript/aula_revisao-Estr_repeticao/script.js
@@ -0,0 +1,26 @@
+// while
+/*
+
+let play = true
+while(play) {
+    //fazer algo a´te o play ser falso
+    //criar um lógica para o play se tornar falso
+    // se não, loop eterno
+}
+*/
+
+// for (tradicional)
+let text = "abc"
+for(let i =  0; i < text.length
+    ; i++) {
+        alert(text[i])
+    }
+
+
+    //for...of
+    for(let char of text) {
+        alert(char)
+    }
+
+
+
