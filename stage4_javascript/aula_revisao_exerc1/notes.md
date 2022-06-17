b1110c5164759c0cd259ccd1ff6529057c23ff5d
mignez <93382579+profamignez@users.noreply.github.com>
2022-06-17 10:49:46 -0300

    Stage 4 - Exercícios

---
+++ stage4_javascript/aula_revisao_exerc1/notes.md
@@ -0,0 +1,10 @@
+Estrutura de Dados
+
+# Ferramentas da linguagem que abordaremos
+
+[] Operadores:
+ 
+Operadores matemáticos
+=== igual no valor e do mesmo tipo (estritamente igual)
+"2" !=== 2
+false (string e Number)
---
+++ stage4_javascript/aula_revisao_exerc1/script.js
@@ -0,0 +1,43 @@
+/*
+
+Solicitar ao usuário que ele insira dois números e, a partir daí, calcular:
+
+• A soma dos dois números;
+• A subtração dos dois números;
+• A multiplicação dos dois números;
+• A divisão dos dois números;
+• O resto da divisão dos dois números;
+
+*/
+
+
+let firstNumber = prompt ("Digite o primeiro número: ")
+let secondNumber = prompt ("Digite o segundo número: ")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const multi = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert("Soma: " + sum)
+alert("Subtração: " + sub)
+alert("Multiplicação: " + multi)
+alert("Divisão: " + div)
+alert("Resto da Divisão: " + restDiv)
+
+const sum2 = (firstNumber + secondNumber) / 2
+
+let result = sum2
+
+if (result) {
+  alert ("Esse número é: " + sum2 + "par")
+} else {
+  alert ("Esse número é: " + sum2 + "impar")
+
+}
+
+
